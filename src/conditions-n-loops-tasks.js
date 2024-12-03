653e1b5ff3f89d11fb4ffed40756fe9ff2badab8
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2024-12-03 22:29:46 +0100

    feat: solve getMaxNumber

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -38,8 +38,14 @@ function isPositive(number) {
  *  -5, 0, 5      => 5
  *  -0.1, 0, 0.2  => 0.2
  */
-function getMaxNumber(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getMaxNumber(a, b, c) {
+  let result = a;
+  if (a < b) {
+    result = b;
+  } else if (a < c) {
+    result = c;
+  }
+  return result;
 }
 
 /**

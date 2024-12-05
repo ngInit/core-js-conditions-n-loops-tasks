af64babcd3c0b9c7cf59c4c78e03b97fc21ef857
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2024-12-05 02:22:08 +0100

    feat: solve canQueenCaptureKing

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -66,8 +66,11 @@ function getMaxNumber(a, b, c) {
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  */
-function canQueenCaptureKing(/* queen, king */) {
-  throw new Error('Not implemented');
+function canQueenCaptureKing(queen, king) {
+  const onSameRow = queen.x === king.x;
+  const onSameCol = queen.y === king.y;
+  const onSameDiag = Math.abs(queen.x - king.x) === Math.abs(queen.y - king.y);
+  return onSameRow || onSameCol || onSameDiag;
 }
 
 /**
